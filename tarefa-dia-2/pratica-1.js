fa60092a95e21c9a67031c88b546422b8fb076dc
guigax22 <gs164994@gmail.com>
2022-08-28 21:22:07 -0300

    answers

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,8 +1,15 @@
-let name = String;
-let email = String;
-let profissao = String;
-let idade = Number;
-let lugar = String
+let nome = 'Guilherme';
+let email = 'fs164994@gmail.com';
+let profissao = 'estudante';
+let idade = 21;
+let lugar = 'Cape Town';
+
+console.log(nome);
+console.log(email);
+console.log(profissao);
+console.log(idade);
+console.log(lugar);
+
 
 
 
